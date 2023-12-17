692bc4b1c76127e20cb4086744fd3ef181c4238a
Alaa Elhadi <alaaelhadi28@gmail.com>
2023-12-17 22:21:38 +0200

    first commit

---
+++ resources/views/kider.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('includes.head')
+
+<body>
+    <div class="container-xxl bg-white p-0">
+        
+    @include('includes.spinner')
+
+    @include('includes.navbar')
+
+    @include('includes.carousel')
+        
+    @include('includes.facilities')
+
+    @include('includes.about')
+
+    @include('includes.action')
+
+    @include('includes.classes')
+    
+    @include('includes.appointment')
+
+    @include('includes.team')  
+
+    @include('includes.testimonial') 
+        
+    @include('includes.footer') 
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+    </div>
+
+    @include('includes.footerJS')   
+
+</body>
+
+</html>
