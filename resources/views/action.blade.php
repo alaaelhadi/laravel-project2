914e890f257e3712de8f9f44569c4cb64a92da6a
Alaa Elhadi <alaaelhadi28@gmail.com>
2023-12-19 19:24:22 +0200

    Second Commit

--- resources/views/action.blade.php
+++ resources/views/action.blade.php
@@ -3,5 +3,19 @@
 Become A Teacher
 @endsection
 @section('content')
+<!-- Page Header End -->
+<div class="container-xxl py-5 page-header position-relative mb-5">
+    <div class="container py-5">
+        <h1 class="display-2 text-white animated slideInDown mb-4">Become A Teachers</h1>
+        <nav aria-label="breadcrumb animated slideInDown">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{ route('index') }}">Home</a></li>
+                <li class="breadcrumb-item"><a href="#">Pages</a></li>
+                <li class="breadcrumb-item text-white active" aria-current="page">Become A Teachers</li>
+            </ol>
+        </nav>
+    </div>
+</div>
+<!-- Page Header End -->
 @include('includes.action')
 @endsection
